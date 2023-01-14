f433abfd27354ac7cc0b1c73be02f35813b8fff5
frankchang <frankchang@hitrust.com.tw>
2023-01-14 11:09:34 +0800

    [新增] 新增圖片上傳功能

--- src/main/java/com/example/springboot/controller/FileController.java
+++ src/main/java/com/example/springboot/controller/FileController.java
@@ -18,7 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,6 +49,35 @@ public class FileController {
 		
 		service.showFile(httpServletRequest, httpServletResponse);
 		
+	}
+	
+	/***
+	 * 將圖片轉換為 string
+	 * @return
+	 * @throws IOException
+	 */
+	@GetMapping(value = "file2String")
+	@ResponseBody
+	public String file2String() throws IOException {
+		
+		String filtString = service.file2String();
+		
+		return filtString;
+	}
+	
+	/***
+	 * 將 string 轉換成 圖片  (待顯示方式)
+	 * @param string
+	 * @param httpServletRequest
+	 * @param httpServletResponse
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@PostMapping(value = "string2file")
+	@ResponseBody
+	public void String2file(@RequestBody String string, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+		
+		service.string2File(string, httpServletRequest, httpServletResponse);
 	}
 	
 }
--- src/main/java/com/example/springboot/service/FileService.java
+++ src/main/java/com/example/springboot/service/FileService.java
@@ -1,5 +1,5 @@
 /**
- * @Description : TODO
+ * @Description : 檔案管理 業務邏輯
  * @ClassName : FileService.java
  * @Copyright : Copyright (c) 2023 
  * @ModifyHistory : 
@@ -11,16 +11,17 @@ package com.example.springboot.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.springboot.handler.ImageResourceHttpRequestHandler;
 
@@ -75,6 +76,40 @@ public class FileService {
         httpServletRequest.setAttribute(ImageResourceHttpRequestHandler.ATTRIBUTE_FILE, file);
         imageResourceHttpRequestHandler.handleRequest(httpServletRequest, httpServletResponse);
 		
+	}
+
+	/**
+	 * 將圖片轉換成字串回傳
+	 * 
+	 * @return
+	 * @throws IOException 
+	 */
+	public String file2String() throws IOException {
+		
+		File file = new File(filePath + "update.png");
+		byte[] fileContent = FileUtils.readFileToByteArray(file);
+		String encodeToString = Base64.getEncoder().encodeToString(fileContent);
+		
+		return encodeToString;
+	}
+
+
+	/***
+	 * 將文字轉換成圖片
+	 * @param string
+	 * @param httpServletRequest
+	 * @param httpServletResponse
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public void string2File(String string, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+
+		byte[] decode = Base64.getDecoder().decode(string);
+		File outFile = new File(filePath + "show.png");
+		FileUtils.writeByteArrayToFile(outFile, decode);
+		
+        httpServletRequest.setAttribute(ImageResourceHttpRequestHandler.ATTRIBUTE_FILE, outFile);
+        imageResourceHttpRequestHandler.handleRequest(httpServletRequest, httpServletResponse);
 	}
 
 }
